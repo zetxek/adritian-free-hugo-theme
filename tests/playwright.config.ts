01002eaeb931af72082ddf98b70077266eae1fd8
Adrián Moreno Peña <adrian.moreno.pena@sumup.com>
2025-01-04 21:53:06 +0700

    Save test results

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -5,7 +5,13 @@ const config: PlaywrightTestConfig = {
     use: {
         baseURL: 'http://localhost:1313',
         screenshot: 'on',
+        trace: 'retain-on-failure',
     },
+    reporter: [
+        ['html', { outputFolder: 'playwright-report' }],
+        ['list']
+    ],
+    outputDir: 'test-results',
     webServer: {
         command: 'cd exampleSite && hugo server --themesDir ../.. --buildDrafts --buildFuture --bind 0.0.0.0',
         url: 'http://localhost:1313',
