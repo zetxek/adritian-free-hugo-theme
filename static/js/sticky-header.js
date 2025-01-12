327bce2f385fb2a2c38d817039bafd951efbe063
Adrián Moreno Peña <adrian.moreno.pena@sumup.com>
2025-01-12 19:11:06 +0700

    Resize handler to show content

--- static/js/sticky-header.js
+++ static/js/sticky-header.js
@@ -5,28 +5,40 @@ class StickyHeader {
       (this.thresholdPosition = 15),
       (this.triggeredStickyClass = "header--sticky-triggered"),
       (this.stickyClass = "header--sticky"),
-      (this.ticking = !1),
+      (this.ticking = false),
       (this.bodyPosition = 0),
       window.addEventListener("DOMContentLoaded", () => this.initSticky()),
       this.scrollChanged();
+      // add window resize listener
+      window.addEventListener("resize", () => this.resizeHandler());
   }
   initSticky() {
     (this.headerStaticHeight = this.header.getBoundingClientRect().height),
-      this.header.classList.toggle(this.stickyClass, !0),
+      this.header.classList.toggle(this.stickyClass, true),
       window.addEventListener("scroll", () => this.scrollHandler());
+      
   }
   scrollHandler() {
     this.ticking ||
       (window.requestAnimationFrame(() => {
-        this.scrollChanged(), (this.ticking = !1);
+        this.scrollChanged(), (this.ticking = false);
       }),
-      (this.ticking = !0));
+      (this.ticking = true));
   }
   scrollChanged() {
     (this.bodyPosition = Math.abs(this.body.getBoundingClientRect().top)),
       this.bodyPosition > this.thresholdPosition
-        ? this.header.classList.toggle(this.triggeredStickyClass, !0)
-        : this.header.classList.toggle(this.triggeredStickyClass, !1);
+        ? this.header.classList.toggle(this.triggeredStickyClass, true)
+        : this.header.classList.toggle(this.triggeredStickyClass, false);
+  }
+  resizeHandler() {
+    console.log('window resized - sticky header. window size = ', window.innerWidth);
+    // if the window size is larger than 992px, remove collapse and show classes
+    // to show the main content in case that the navigation was triggered
+    if (window.innerWidth > 991) {
+        this.header.classList.remove('show');
+        this.header.classList.remove('collapsed');
+      }
   }
 }
 const stickyHeader = new StickyHeader(document.querySelector(".header"));
